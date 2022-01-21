df42932757351c65c01b104acfd3d9fb24460cde
Payal Kunwar <74529731+Payal-Kunwar@users.noreply.github.com>
2022-01-21 12:27:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 //const api_url = "<heroku_app_url>"
-const api_url = "https://myg5esd.herokuapp.com/"
-//const api_url = "<heroku_app_url>"
-//const api_url = "localhost:8080/user"
+const api_url = "https://hrgrp.herokuapp.com/user"
 
 function loadData(records = []) {
 	var table_data = "";
@@ -10,9 +8,14 @@ function loadData(records = []) {
 		table_data += `<td>${records[i].name}</td>`;
 		table_data += `<td>${records[i].age}</td>`;
 		table_data += `<td>${records[i].city}</td>`;
+		table_data += `<td>${records[i].house_no}</td>`;
+		table_data += `<td>${records[i].no_of_rooms}</td>`;
+		table_data += `<td>${records[i].duration_in_months}</td>`;
+		table_data += `<td>${records[i].rent}</td>`;
+
 		table_data += `<td>`;
 		table_data += `<a href="edit.html?id=${records[i]._id}"><button class="btn btn-primary">Edit</button></a>`;
-		table_data += '&nbsp;&nbsp;';
+		table_data += '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
 		table_data += `<button class="btn btn-danger" onclick=deleteData('${records[i]._id}')>Delete</button>`;
 		table_data += `</td>`;
 		table_data += `</tr>`;
@@ -41,16 +44,51 @@ function getDataById(id) {
 		document.getElementById("name").value = data.name;
 		document.getElementById("age").value = data.age;
 		document.getElementById("city").value = data.city;
+		document.getElementById("house_no").value = data.house_no;
+		document.getElementById("no_of_rooms").value = data.no_of_rooms;
+		document.getElementById("duration_in_months").value = data.duration_in_months;
+		document.getElementById("rent").value = data.rent;
+
 	})
 }
 
 
+/*function postData() {
+	var name = document.getElementById("name").value;
+	var age = document.getElementById("age").value;
+	var city = document.getElementById("city").value;
+	var house_no = document.getElementById("house_no").value;
+	var no_of_rooms = document.getElementById("no_of_rooms").value;
+	var duration_in_months = document.getElementById("duration_in_months").value;
+	var rent = document.getElementById("rent").value;
+	
+	data = {name: name, age: age, city: city, house_no: house_no, no_of_rooms: no_of_rooms, duration_in_months: duration_in_months, rent: rent};
+	
+	fetch(api_url, {
+		method: "POST",
+		headers: {
+		  'Accept': 'application/json',
+		  'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(data)
+	})
+	.then((response) => response.json())
+	.then((data) => { 
+		console.log(data); 
+		window.location.href = "index.html";
+	})
+}	
+*/
 function postData() {
 	var name = document.getElementById("name").value;
 	var age = document.getElementById("age").value;
 	var city = document.getElementById("city").value;
+	var house_no = document.getElementById("house_no").value;
+	var no_of_rooms = document.getElementById("no_of_rooms").value;
+	var duration_in_months = document.getElementById("duration_in_months").value;
+	var rent = document.getElementById("rent").value;
 	
-	data = {name: name, age: age, city: city};
+	data = {name: name, age: age, city: city, house_no: house_no, no_of_rooms: no_of_rooms, duration_in_months: duration_in_months, rent: rent};
 	
 	fetch(api_url, {
 		method: "POST",
@@ -74,9 +112,13 @@ function putData() {
 	var name = document.getElementById("name").value;
 	var age = document.getElementById("age").value;
 	var city = document.getElementById("city").value;
-	
-	data = {_id: _id, name: name, age: age, city: city};
-	
+	var house_no = document.getElementById("house_no").value;
+	var no_of_rooms = document.getElementById("no_of_rooms").value;
+	var duration_in_months = document.getElementById("duration_in_months").value;
+	var rent = document.getElementById("rent").value;
+
+	data = {_id: _id, name: name, age: age, city: city, house_no: house_no, no_of_rooms: no_of_rooms, duration_in_months: duration_in_months, rent: rent};
+
 	fetch(api_url, {
 		method: "PUT",
 		headers: {
